56a98e08375a6b3684ca28941eaab039fa38ef8f
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-17 14:43:21 +0800

    fix: 强制刷新缓存 xinye-v7→v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-v7';
+const CACHE_NAME = 'xinye-v8';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/jszip.min.js'];
 
 self.addEventListener('install', e => {
