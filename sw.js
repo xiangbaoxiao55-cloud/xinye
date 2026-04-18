8afd2efa09448308e6d3b8434c2f41e272f8e21f
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-18 10:52:57 +0800

    fix: 共读批注查看/编辑分离 + 💬按钮不再嵌套导航栏

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260418-1036';
+const CACHE_NAME = 'xinye-20260418-1052';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/jszip.min.js'];
 
 self.addEventListener('install', e => {
