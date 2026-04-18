f5e5d4b609b832705fb3d4893c6698a51ad0f5a0
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-18 11:11:50 +0800

    feat: 问炘也带完整聊天历史40条+回复存入聊天记录 + SW切回前台自动检查更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260418-1052';
+const CACHE_NAME = 'xinye-20260418-1111';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/jszip.min.js'];
 
 self.addEventListener('install', e => {
