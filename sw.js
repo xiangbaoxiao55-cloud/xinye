4c6c277783c9953a1cf879c1578bc5612fa18851
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-18 16:03:18 +0800

    fix: 整理记忆档案支持主API备用预设failover + 防止错误内容覆写档案

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260418-1424';
+const CACHE_NAME = 'xinye-20260418-1603';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/jszip.min.js'];
 
 self.addEventListener('install', e => {
