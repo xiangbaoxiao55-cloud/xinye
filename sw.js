7f0a0df051c32419054c0b027c668a16430595ea
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-19 15:48:00 +0800

    fix: 收藏面板只有点头像才打开

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260419-1009';
+const CACHE_NAME = 'xinye-20260419-1548';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/jszip.min.js'];
 
 self.addEventListener('install', e => {
