e8c3db0b02a7c0e083b7266503b2b3f2d11c19aa
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-19 16:15:54 +0800

    fix: digestMemory 加 max_tokens:8000 + 去掉多余 systemPrompt，防止档案被截断

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260419-1612';
+const CACHE_NAME = 'xinye-20260419-1615';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/jszip.min.js'];
 
 self.addEventListener('install', e => {
