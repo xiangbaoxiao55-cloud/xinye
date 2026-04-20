38c3725c3bf1656f9b58704498d69a66c0beb475
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-20 11:12:34 +0800

    fix: 导入备份时chatStickers超localStorage配额不再崩溃，跳过并提示

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260420-1049';
+const CACHE_NAME = 'xinye-20260420-1112';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/jszip.min.js'];
 
 self.addEventListener('install', e => {
