10a3ec56885e7e2517e9653003a86223aa639f90
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-20 18:59:37 +0800

    feat: vConsole候选池日志加有向量条数

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260420-1856';
+const CACHE_NAME = 'xinye-20260420-1859';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/jszip.min.js'];
 
 self.addEventListener('install', e => {
