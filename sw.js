ebf7c8063e4f70ff25f215782744e87022c1cb11
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-21 19:21:22 +0800

    feat: 炘也speak工具 + 微信风格语音条（炘也可主动选择语音回复）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260420-2133';
+const CACHE_NAME = 'xinye-20260421-1921';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/jszip.min.js'];
 
 self.addEventListener('install', e => {
