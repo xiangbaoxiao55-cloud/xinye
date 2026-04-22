5348743e9871b3fc7ba66502f6b02093ab35716e
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-22 18:19:10 +0800

    fix: 画图兼容多种返回格式，加vConsole日志方便排查

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260422-1814';
+const CACHE_NAME = 'xinye-20260422-1819';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/jszip.min.js'];
 
 self.addEventListener('install', e => {
