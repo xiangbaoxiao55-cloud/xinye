bbd637d1090ecb83e1e0f4f4674fdc5ccd969aeb
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-23 11:19:42 +0800

    fix: speak工具执行后直接结束，不发第二次API请求

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260423-1106';
+const CACHE_NAME = 'xinye-20260423-1119';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/lib/jszip.min.js'];
 
 self.addEventListener('install', e => {
