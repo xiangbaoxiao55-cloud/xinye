6dc2bac33e972dba87a08310f008020e77f0951b
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-23 23:15:31 +0800

    fix: TTS/stripThinking 覆盖<think>短标签，不再读出思考链

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260423-1616';
+const CACHE_NAME = 'xinye-20260423-2315';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/lib/jszip.min.js'];
 
 self.addEventListener('install', e => {
