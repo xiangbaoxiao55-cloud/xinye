c263bc382e139e76d08e3bb8f67bfe0c5313344d
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-24 11:31:20 +0800

    fix: 音色预设保存/激活补全MiniMax字段（voiceId/key/groupId/model/proxy）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260424-1012';
+const CACHE_NAME = 'xinye-20260424-1131';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/lib/jszip.min.js'];
 
 self.addEventListener('install', e => {
