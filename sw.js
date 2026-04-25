13930e3323122b163f5750b64efdbf2c12d57177
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-25 20:04:49 +0800

    fix: file://打开时自动重定向到localhost:8787（ES module不支持file://协议）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260425-2000';
+const CACHE_NAME = 'xinye-20260425-2004';
 const LOCAL_CFG  = 'xinye-local-cfg';
 const STATIC_ASSETS = [
   '/', '/index.html', '/diary.html', '/reading.html', '/lib/jszip.min.js',
