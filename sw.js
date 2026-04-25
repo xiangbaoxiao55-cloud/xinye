7371207eeef536255ee96c767de155b7de4ecb64
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-25 09:14:59 +0800

    feat: 朋友聊天加一键整理记忆按钮，AI自动从对话提取关键信息

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260425-0913';
+const CACHE_NAME = 'xinye-20260425-0914';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/lib/jszip.min.js'];
 
 self.addEventListener('install', e => {
