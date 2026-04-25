bb764857353419ad8841ad2f0412e836b44e9953
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-25 08:54:58 +0800

    feat: 朋友tab加换行按钮/记忆/历史条数配置/API预设导入

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260425-0840';
+const CACHE_NAME = 'xinye-20260425-0854';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/lib/jszip.min.js'];
 
 self.addEventListener('install', e => {
