3be61e5932b08462ce8f805853bb3909e7b21264
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-25 10:52:28 +0800

    feat: 朋友聊天气泡加复制按钮

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260425-1033';
+const CACHE_NAME = 'xinye-20260425-1052';
 const STATIC_ASSETS = ['/', '/index.html', '/diary.html', '/reading.html', '/lib/jszip.min.js'];
 
 self.addEventListener('install', e => {
