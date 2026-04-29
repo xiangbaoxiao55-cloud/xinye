3ae240c032e9483ca95df74425f22419b1815be4
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-04-29 22:01:46 +0800

    fix: 记忆查看器「炘也自记」按钮文字缩短为「自记」，移动端不挤行

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260429-2200';
+const CACHE_NAME = 'xinye-20260429-2201';
 const LOCAL_CFG  = 'xinye-local-cfg';
 const STATIC_ASSETS = [
   '/', '/index.html', '/diary.html', '/reading.html', '/lib/jszip.min.js',
