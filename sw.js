6465af16e754906df75011418ef64003e11f18dd
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-05-04 11:22:42 +0800

    feat: 群聊自发对话按钮+SKIP加强+提示词去和谐化

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260504-1107';
+const CACHE_NAME = 'xinye-20260504-1122';
 const LOCAL_CFG  = 'xinye-local-cfg';
 const STATIC_ASSETS = [
   '/', '/index.html', '/choubao.html', '/choubao.webmanifest', '/diary.html', '/reading.html', '/lib/jszip.min.js',
