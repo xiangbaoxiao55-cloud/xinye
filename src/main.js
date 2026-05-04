89b8574783f12bcaa04eb1b071bbca886f17b20b
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-05-04 11:42:18 +0800

    feat: 群聊用户可@炘也/@臭宝定向发消息

--- src/main.js
+++ src/main.js
@@ -373,7 +373,7 @@ async function checkPendingMessage() {
 (async () => {
   // 显示版本号
   const _verEl = document.getElementById('appVersion');
-  if (_verEl) _verEl.textContent = 'v2026.05.04-1132';
+  if (_verEl) _verEl.textContent = 'v2026.05.04-1142';
 
   await openDB();
   await migrateFromLocalStorage();
