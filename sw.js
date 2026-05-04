403e617a37d765f485154ff5fcbca68fc44f8cb1
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-05-04 15:44:58 +0800

    feat: 移除两边RP模块图标和面板

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260504-1142';
+const CACHE_NAME = 'xinye-20260504-1544';
 const LOCAL_CFG  = 'xinye-local-cfg';
 const STATIC_ASSETS = [
   '/', '/index.html', '/choubao.html', '/choubao.webmanifest', '/diary.html', '/reading.html', '/lib/jszip.min.js',
