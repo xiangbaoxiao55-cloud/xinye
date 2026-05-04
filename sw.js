e7c3a7f687a93f26554a1128976ee9f29241e2e7
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-05-04 11:27:29 +0800

    fix: 戳一戳改用system指令让他俩互相说话而非对涂涔

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260504-1122';
+const CACHE_NAME = 'xinye-20260504-1127';
 const LOCAL_CFG  = 'xinye-local-cfg';
 const STATIC_ASSETS = [
   '/', '/index.html', '/choubao.html', '/choubao.webmanifest', '/diary.html', '/reading.html', '/lib/jszip.min.js',
