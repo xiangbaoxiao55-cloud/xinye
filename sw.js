e91aa82ccad362bd6eb607bf58c56588b8b2311f
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-05-06 21:38:59 +0800

    fix: 画图超时调整为300s

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260506-2134';
+const CACHE_NAME = 'xinye-20260506-2138';
 const LOCAL_CFG  = 'xinye-local-cfg';
 const STATIC_ASSETS = [
   '/', '/index.html', '/choubao.html', '/choubao.webmanifest', '/diary.html', '/reading.html', '/lib/jszip.min.js',
