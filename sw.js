46fcb4cae38de05e6a1e929d569c23189b273c64
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-05-07 23:54:13 +0800

    fix: diary loadCfg读key从cfg改main，修复炘也看看提示需配置API的bug

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260507-2334';
+const CACHE_NAME = 'xinye-20260507-2354';
 const LOCAL_CFG  = 'xinye-local-cfg';
 const STATIC_ASSETS = [
   '/', '/index.html', '/choubao.html', '/choubao.webmanifest', '/diary.html', '/reading.html', '/lib/jszip.min.js',
