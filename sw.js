09b8f506cb8b2c488420e11f6391c236df92a19a
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-05-29 00:39:55 +0800

    fix: draw 默认尺寸改为 1536x2048 竖2K

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260528-2229';
+const CACHE_NAME = 'xinye-20260529-0039';
 const LOCAL_CFG  = 'xinye-local-cfg';
 const STATIC_ASSETS = [
   '/', '/index.html', '/choubao.html', '/choubao.webmanifest', '/diary.html', '/reading.html', '/lib/jszip.min.js',
