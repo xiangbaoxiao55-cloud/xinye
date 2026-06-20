58ba59c71fa7d421eb373ca4f92a42f0e5e05a1c
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-06-20 18:40:29 +0800

    fix: 模型选择浮层搜索框加暗夜模式背景色

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260620-1825';
+const CACHE_NAME = 'xinye-20260620-1840';
 const LOCAL_CFG  = 'xinye-local-cfg';
 const STATIC_ASSETS = [
   '/', '/index.html', '/choubao.html', '/choubao.webmanifest', '/diary.html', '/reading.html', '/lib/jszip.min.js',
