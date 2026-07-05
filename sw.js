0871ede4099f938bab60d16fcf310f75360e0b5c
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-07-06 00:46:40 +0800

    故事板：项目列表页加书架入口按钮

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xinye-20260705-2130';
+const CACHE_NAME = 'xinye-20260706-0046';
 const LOCAL_CFG  = 'xinye-local-cfg';
 const STATIC_ASSETS = [
   '/', '/index.html', '/choubao.html', '/choubao.webmanifest', '/diary.html', '/reading.html', '/lib/jszip.min.js',
