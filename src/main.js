e626de08e60067090109780ffa1cf5a26aff91d0
xiangbaoxiao55-cloud <xiangbaoxiao55@github.com>
2026-07-15 19:59:44 +0800

    feat: 画图台尺寸下拉新增 2560x1440 / 1440x2560

--- src/main.js
+++ src/main.js
@@ -389,7 +389,7 @@ async function checkPendingMessage() {
 (async () => {
   // 显示版本号
   const _verEl = document.getElementById('appVersion');
-  if (_verEl) _verEl.textContent = 'v2026.07.15-1938';
+  if (_verEl) _verEl.textContent = 'v2026.07.15-1959';
 
   await openDB();
   await migrateFromLocalStorage();
